ec31b51cab9130538f2973185bf2ddff0ab21ef7
sairaj <sairajsrigiri@gmail.com>
2025-04-16 00:10:07 +0530

    frontend part

---
+++ client/src/App.jsx
@@ -0,0 +1,49 @@
+import styled, { ThemeProvider } from "styled-components"
+import { darkTheme } from "./utils/Theme"
+import Home from "./pages/Home";
+import CreatePost from "./pages/CreatePost";
+import {BrowserRouter, Route, Routes} from "react-router-dom"
+import Navbar from "./components/Navbar";
+import GeneratedImageCard from "./components/GenerateImageForm";
+
+const Container = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  background: ${({ theme }) => theme.bg};
+  color: ${({ theme }) => theme.text_primary};
+  overflow-x: hidden;
+  overflow-y: hidden;
+  transition: all 0.2s ease;
+`;
+const Wrapper = styled.div`
+  height: 100%;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  flex: 3;
+`;
+
+function App() {
+
+
+  return (
+    <ThemeProvider theme={darkTheme}>
+      <Container>
+        <Wrapper>
+          <BrowserRouter>
+            <Navbar />
+            <GeneratedImageCard />
+            <Routes>
+              <Route path="/" element={<Home/>}/>
+              <Route path="/post" element={<CreatePost/>}/>
+            </Routes>
+          </BrowserRouter>
+        </Wrapper>
+      </Container>
+    </ThemeProvider> 
+  )
+}
+
+export default App
